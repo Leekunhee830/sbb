650f689d0bc530244c7cb1bf211cfbe13d189ce3
Leekunhee830 <gorrka159@naver.com>
2022-04-30 16:46:52 +0900

    내비게이션바,페이징처리

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -4,6 +4,7 @@ import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.answer.AnswerRepository;
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionRepository;
+import com.mysite.sbb.question.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,103 +19,115 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest
 class SbbApplicationTests {
 
-	@Autowired
-	private QuestionRepository questionRepository;
-	@Autowired
-	private AnswerRepository answerRepository;
+    @Autowired
+    private QuestionRepository questionRepository;
+    @Autowired
+    private AnswerRepository answerRepository;
+    @Autowired
+    private QuestionService questionService;
 
-	@Test
-	void testCreateAnswer(){
-		Optional<Question> oq = this.questionRepository.findById(2);
-		assertTrue(oq.isPresent());
-		Question q = oq.get();
+    @Test
+    void testCreateAnswer() {
+        Optional<Question> oq = this.questionRepository.findById(2);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
 
-		Answer a = new Answer();
-		a.setContent("네 자동으로 생성됩니다.");
-		a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
-		a.setCreateDate(LocalDateTime.now());
-		this.answerRepository.save(a);
-	}
+        Answer a = new Answer();
+        a.setContent("네 자동으로 생성됩니다.");
+        a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
+        a.setCreateDate(LocalDateTime.now());
+        this.answerRepository.save(a);
+    }
 
-	@Test
-	void testFindBySubjectAndContent(){
-		Question q = this.questionRepository.findBySubjectAndContent(
-				"sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
-		assertEquals(1, q.getId());
-	}
+    @Test
+    void testFindBySubjectAndContent() {
+        Question q = this.questionRepository.findBySubjectAndContent(
+                "sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+        assertEquals(1, q.getId());
+    }
 
-	@Test
-	void testFindBySubject(){
-		Question q= questionRepository.findBySubject("sbb가 무엇인가요?").get();
-		assertEquals(q.getId(),1);
-	}
+    @Test
+    void testFindBySubject() {
+        Question q = questionRepository.findBySubject("sbb가 무엇인가요?").get();
+        assertEquals(q.getId(), 1);
+    }
 
-	@Test
-	void testFindById(){
-		Optional<Question> oq=questionRepository.findById(1);
+    @Test
+    void testFindById() {
+        Optional<Question> oq = questionRepository.findById(1);
 
-		if(oq.isPresent()){
-			Question q=oq.get();
-			assertEquals("sbb가 무엇인가요?", q.getSubject());
-		}
+        if (oq.isPresent()) {
+            Question q = oq.get();
+            assertEquals("sbb가 무엇인가요?", q.getSubject());
+        }
 
-	}
+    }
 
-	@Test
-	void testDelete(){
-		assertEquals(2,questionRepository.count());
-		Optional<Question> oq=this.questionRepository.findById(1);
-		assertTrue(oq.isPresent());
-		Question q=oq.get();
-		this.questionRepository.delete(q);
-		assertEquals(1,this.questionRepository.count());
-	}
+    @Test
+    void testDelete() {
+        assertEquals(2, questionRepository.count());
+        Optional<Question> oq = this.questionRepository.findById(1);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+        this.questionRepository.delete(q);
+        assertEquals(1, this.questionRepository.count());
+    }
 
-	@Test
-	void testModify(){
-		Optional<Question> oq=questionRepository.findById(1);
-		assertTrue(oq.isPresent());
-		Question q=oq.get();
-		q.setSubject("수정된 제목");
-		this.questionRepository.save(q);
+    @Test
+    void testModify() {
+        Optional<Question> oq = questionRepository.findById(1);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+        q.setSubject("수정된 제목");
+        this.questionRepository.save(q);
 
-	}
+    }
 
-	@Test
-	void testFindBySubjectLike(){
-		List<Question> qList=this.questionRepository.findBySubjectLike("sbb%");
-		Question q=qList.get(0);
-		assertEquals("sbb가 무엇인가요?",q.getSubject());
-	}
+    @Test
+    void testFindBySubjectLike() {
+        List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+        Question q = qList.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
+    }
 
 
-	@Test
-	void testFindAll(){
-		List<Question> all=this.questionRepository.findAll();;
-		assertEquals(2, all.size());
+    @Test
+    void testFindAll() {
+        List<Question> all = this.questionRepository.findAll();
+        ;
+        assertEquals(2, all.size());
 
-		Question q = all.get(0);
-		assertEquals("sbb가 무엇인가요?", q.getSubject());
-	}
+        Question q = all.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
+    }
 
-	@Test
-	void testCreateQuestions(){
-		Question q1=new Question();
-		q1.setSubject("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해서 알고 싶습니다.");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);  // 첫번째 질문 저장
+    @Test
+    void testCreateQuestions() {
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해서 알고 싶습니다.");
+        q1.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q1);  // 첫번째 질문 저장
 
-		Question q2 = new Question();
-		q2.setSubject("스프링부트 모델 질문입니다.");
-		q2.setContent("id는 자동으로 생성되나요?");
-		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);  // 두번째 질문 저장
-	}
+        Question q2 = new Question();
+        q2.setSubject("스프링부트 모델 질문입니다.");
+        q2.setContent("id는 자동으로 생성되나요?");
+        q2.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q2);  // 두번째 질문 저장
+    }
 
-	@Test
-	void contextLoads() {
+    @Test
+    void contextLoads() {
 
-	}
+    }
+
+    @Test
+    void testData() {
+        for (int i = 1; i <= 300; i++) {
+            String subject = String.format("테스트 데이터입니다:[%03d]", i);
+            String content = "내용무";
+            this.questionService.create(subject, content);
+        }
+    }
 
 }
